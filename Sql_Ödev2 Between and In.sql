0af183330fb819fc7dae6ab46f8052af7e24be64
Muhammed Emin ÖZDEMİR <80462839+MuhammedEminOzdemir@users.noreply.github.com>
2022-12-17 11:51:36 +0300

    Add files via upload

---
+++ Sql_Ödev2 Between and In.sql
@@ -0,0 +1,21 @@
+--1
+/*film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 
+12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız 
+( BETWEEN - AND yapısını kullanınız.)*/
+Select replacement_cost from film where replacement_cost Between 12.99 and 16.99
+
+---------------------------------------------------------------------------------------------------------
+
+--2 actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 
+--'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)
+select first_name,last_name from actor where first_name IN('Penelope','Nick','Ed');
+
+---------------------------------------------------------------------------------------------------------
+
+--3 film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99
+--VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)
+select *from film where rental_rate IN(0.99, 2.99, 4.99) AND replacement_cost IN(12.99, 15.99, 28.99)
+
+
+
+
